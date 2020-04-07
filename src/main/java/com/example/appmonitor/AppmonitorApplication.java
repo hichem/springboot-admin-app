984b8c16b1225d783cf68bcb56023135a7079c06
Hichem BOUSSETTA <boussettahichem@yahoo.fr>
2020-04-07 14:28:20 +0200

    Added azure ad authentication

--- src/main/java/com/example/appmonitor/AppmonitorApplication.java
+++ src/main/java/com/example/appmonitor/AppmonitorApplication.java
@@ -2,9 +2,10 @@ package com.example.appmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Configuration;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+@Configuration
 @EnableAdminServer
 @SpringBootApplication
 public class AppmonitorApplication {
