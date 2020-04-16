3663d796653a9684d1781d7dbddf8290dc243ada
Hichem BOUSSETTA <boussettahichem@yahoo.fr>
2020-04-16 11:16:59 +0200

    [AIC-2976] Added azure ad authentication & login / logout configuration

--- src/main/java/com/example/appmonitor/WebSecurityConfig.java
+++ src/main/java/com/example/appmonitor/WebSecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 
 //@Configuration
 @EnableWebSecurity
@@ -17,11 +21,41 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
 
+    private final String adminContextPath;
+
+    public WebSecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .anyRequest().authenticated()
+    	
+    	SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+    	
+    	http
+	    	.authorizeRequests()
+		        .antMatchers(adminContextPath + "/assets/**").permitAll() 
+		        .antMatchers(adminContextPath + "/login").permitAll()
+		        .anyRequest().authenticated() 
+            .and()
+            .formLogin()
+            	.loginPage(adminContextPath + "/login")
+            	.successHandler(successHandler)
+            .and()
+            .logout()
+	            .logoutUrl(adminContextPath + "/logout")
+	            .invalidateHttpSession(true)
+	            .clearAuthentication(true)
+	            .logoutSuccessUrl("/")
+	        .and()
+	        .csrf()
+	            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())  
+	            .ignoringAntMatchers(
+	                adminContextPath + "/instances",   
+	                adminContextPath + "/actuator/**"  
+	            )
             .and()
             .oauth2Login()
             .userInfoEndpoint()
